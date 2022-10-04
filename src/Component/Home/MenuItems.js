b14ddf87d38d5165514b511f1b7e42ce8f19e9b5
emmysoft123 <47819331+emmysoft123@users.noreply.github.com>
2022-10-04 13:34:03 +0100

    second commit,please merge

---
+++ src/Component/Home/MenuItems.js
@@ -0,0 +1,29 @@
+import Landing from "../landing/landing"
+export const MenuItems=[
+    {   
+        id:1,
+        title:'00 Home',
+        url:'',
+        cName:'Nav-Link active',
+    
+        
+    },
+    {   
+        id:2,
+        title:'01 Destination',
+        url:'#',
+        cName:'Nav-Link',
+    },
+    {   
+        id:3,
+        title:'02 Crew',
+        url:'#',
+        cName:'Nav-Link',
+    },
+    {   
+        id:4,
+        title:'03 Technology',
+        url:'#',
+        cName:'Nav-Link',
+    },
+]
---
+++ src/Component/landing/text.jsx
@@ -0,0 +1,27 @@
+import './text.css';
+const LandingWriteup=()=>{
+     return(
+         <>
+
+<div className="landing_text">
+            <h4 className="so_text">
+              So You Want To Travel To 
+            </h4>
+            <h1 className='space_text'>
+              SPACE
+            </h1>
+            <p className="space_subText">
+              Let's face it,if you want to go to space.You might as well,<br/>
+              genuinely go to outer space and hover kind of on the <br/>
+              edge of it.Well sit back,and relate because we will give you <br/>
+              a truly out of this world experience<br/>
+            </p>
+            <div className='Explore'>
+                <h1 className="explore_text">EXPLORE</h1>
+            </div>
+          </div>
+
+         </>
+     )
+}
+export default LandingWriteup;
